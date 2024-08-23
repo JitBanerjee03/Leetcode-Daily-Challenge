d7a334e0a41b4492df5435096c1824dd821231d0
JitBanerjee03 <Jitbanerjee294@gmail.com>
2024-08-24 01:34:23 +0530

    first commit

---
+++ Fraction_Addition_and_Subtraction.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        int i=0,numerator=0,denominator=1;
+        bool isnegative;
+
+        while(i<expression.length()){
+            
+            int tempNumerator=0,tempDenominator=0;
+
+            if(expression[i]=='-')
+                isnegative=true;
+        
+            else
+                isnegative=false;
+            
+            i++;
+            
+            while(expression[i]>='0' && expression[i]<='9'){
+                tempNumerator=tempNumerator*10+(expression[i]-'0');
+                i++;
+            }
+
+            i++;
+
+            while(i<expression.length() && expression[i]>='0' && expression[i]<='9'){
+                tempDenominator=tempDenominator*10+(expression[i]-'0');
+                i++;
+            }
+
+            if(isnegative)
+                tempNumerator*=-1;
+
+            numerator=numerator*tempDenominator + tempNumerator*denominator;
+
+            denominator=denominator*tempDenominator;
+        }
+
+        int gcd=abs(__gcd(numerator,denominator));
+
+        numerator/=gcd;
+        denominator/=gcd;
+
+        string result="";
+
+        result=to_string(numerator)+'/'+to_string(denominator);
+
+        return result;
+    }
+};
+
+int main(){
+    Solution Obj;
+
+    cout<<Obj.fractionAddition("-1/2+1/2");  //"-1/2+1/2"
+
+    cout<<endl;
+    
+    cout<<Obj.fractionAddition("-1/2+1/2+1/3");  //"-1/2+1/2+1/3"
+
+    cout<<endl;
+    
+    cout<<Obj.fractionAddition("1/3-1/2");  //1/3-1/2
+}
